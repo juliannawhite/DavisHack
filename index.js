3ba95747ea49e091f8ece19151e2c4fd9736d506
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-01-21 08:56:29 -0800

    pin drop functionality

--- index.js
+++ index.js
@@ -34,7 +34,40 @@ var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([39, -98], 4)
     .addControl(L.mapbox.geocoderControl('mapbox.places', {
         keepOpen: true
-    }));
+    }))
+
+map.doubleClickZoom.disable();
+map.unbind('dblclick');
+map.on('dblclick', function(e) {
+    
+	
+    var randomHex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    L.mapbox.featureLayer({
+    // this feature is in the GeoJSON format: see geojson.org
+    // for the full specification
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        // coordinates here are in longitude, latitude order because
+        // x, y is the standard for GeoJSON and many formats
+        coordinates: [
+          e.latlng.lng,
+          e.latlng.lat
+        ]
+    },
+    properties: {
+        title: 'new',
+        description: 'new',
+        // one can customize markers by adding simplestyle properties
+        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+        'marker-size': 'large',
+        'marker-color': randomHex,
+        'marker-symbol': 'heart'
+    }
+}).addTo(map);
+});
+
+    
 
   L.mapbox.featureLayer({
     // this feature is in the GeoJSON format: see geojson.org
